60569195890e5a161e648c1693368098a403a3f5
mkvlrn <mkvlrn@gmail.com>
2026-08-19 01:20:02 -0300

    fix: better error messages

--- utils/format-error.ts
+++ utils/format-error.ts
@@ -4,10 +4,20 @@ export function formatError(error: unknown): string {
   }
 
   const output = [error.stack ?? error.message];
+
   let current = error.cause;
 
   while (current instanceof Error) {
     output.push(`Caused by:\n${current.stack ?? current.message}`);
+
+    if ("stdout" in current && current.stdout instanceof Uint8Array && current.stdout.length > 0) {
+      output.push(`stdout:\n${Buffer.from(current.stdout).toString().trim()}`);
+    }
+
+    if ("stderr" in current && current.stderr instanceof Uint8Array && current.stderr.length > 0) {
+      output.push(`stderr:\n${Buffer.from(current.stderr).toString().trim()}`);
+    }
+
     current = current.cause;
   }
 

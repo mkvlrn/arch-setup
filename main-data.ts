7836f2a48510a595d3f421018cbe605c004732ec
mkvlrn <mkvlrn@gmail.com>
2026-08-19 01:11:36 -0300

    refactor: use binary

--- main-data.ts
+++ main-data.ts
@@ -3,48 +3,7 @@ import path from "node:path";
 import { $ } from "bun";
 import type { UpdateConfsArgs } from "./scripts/update-confs";
 import type { XdgArgs } from "./scripts/xdg";
-import type { YayArgs } from "./scripts/yay";
-
-export const xdgArgs = {
-  homeDir: os.homedir(),
-  mkdir: ["repos", "sandbox", "work", "documents", "downloads", "downloads/torrents"],
-  rmrf: ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Templates", "Videos"],
-  cmd: { exec: () => $`xdg-user-dirs-update`, name: "xdg dirs update", quiet: true },
-} satisfies XdgArgs;
-
-export const yayArgs = {
-  cmds: [
-    {
-      exec: () => $`git clone https://aur.archlinux.org/yay-bin`.cwd(os.homedir()),
-      name: "clone yay-bin",
-      quiet: true,
-    },
-    {
-      exec: () => $`makepkg -si --noconfirm`.cwd(path.join(os.homedir(), "yay-bin")),
-      name: "make yay",
-      quiet: true,
-    },
-    {
-      exec: () => $`rm -rf ${path.join(os.homedir(), "yay-bin")}`,
-      name: "remove yay-bin src dir",
-      quiet: true,
-    },
-    { exec: () => $`yay -Y --gendb`, name: "track git packages", quiet: true },
-    { exec: () => $`yay -Y --devel --save`, name: "enable dev packages updates", quiet: true },
-    {
-      exec: () =>
-        $`sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist`,
-      name: "get best mirror list",
-      quiet: true,
-    },
-    { exec: () => $`yay -Syu --noconfirm`, name: "update package data", quiet: true },
-    {
-      exec: () => $`yay -Qq | grep -- '-debug$' | xargs -r yay -Rnsu`,
-      name: "remove *-debug packages",
-      quiet: true,
-    },
-  ],
-} satisfies YayArgs;
+import type { Command } from "./utils/run-shell-commands";
 
 export const updateConfsArgs = {
   etcDir: "/etc",
@@ -61,3 +20,92 @@ export const updateConfsArgs = {
   sudoInstal: (origin: string, destination: string) =>
     $`sudo install -m 644 ${origin} ${destination}`,
 } satisfies UpdateConfsArgs;
+
+export const baseArgs = [
+  { exec: () => $`sudo -v`, name: "sudo timestamp", quiet: true },
+  {
+    exec: () => $`sudo pacmain -Syu --noconfirm --needed git base-devel reflector`,
+    name: "install basic packages for yay",
+    quiet: true,
+  },
+] satisfies Command[];
+
+export const yayArgs = [
+  {
+    exec: () => $`git clone https://aur.archlinux.org/yay-bin`.cwd(os.homedir()),
+    name: "clone yay-bin",
+    quiet: true,
+  },
+  {
+    exec: () => $`makepkg -si --noconfirm`.cwd(path.join(os.homedir(), "yay-bin")),
+    name: "make yay",
+    quiet: true,
+  },
+  {
+    exec: () => $`rm -rf ${path.join(os.homedir(), "yay-bin")}`,
+    name: "remove yay-bin src dir",
+    quiet: true,
+  },
+  { exec: () => $`yay -Y --gendb`, name: "track git packages", quiet: true },
+  { exec: () => $`yay -Y --devel --save`, name: "enable dev packages updates", quiet: true },
+  {
+    exec: () =>
+      $`sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist`,
+    name: "get best mirror list",
+    quiet: true,
+  },
+  { exec: () => $`yay -Syu --noconfirm`, name: "update package data", quiet: true },
+  {
+    exec: () => $`yay -Qq | grep -- '-debug$' | xargs -r yay -Rnsu`,
+    name: "remove *-debug packages",
+    quiet: true,
+  },
+] satisfies Command[];
+
+const pkgs = [
+  "brave-bin",
+  "bruno-bin",
+  "deluge-gtk",
+  "deluge",
+  "docker-buildx",
+  "docker-compose",
+  "docker",
+  "fish",
+  "kitty",
+  "less",
+  "okular",
+  "openssh",
+  "power-profiles-daemon",
+  "qalculate-qt",
+  "stow",
+  "ttf-hack-nerd",
+  "ttf-iosevkaterm-nerd",
+  "ttf-zed-mono-nerd",
+  "unzip",
+  "vscodium-bin",
+  "xdg-user-dirs",
+  "zed",
+];
+export const pkgArgs = [
+  {
+    exec: () => $`yay -S --noconfirm --needed ${pkgs.join(" ")}`,
+    name: "update package data",
+    quiet: true,
+  },
+] satisfies Command[];
+
+export const xdgArgs = {
+  homeDir: os.homedir(),
+  mkdir: ["repos", "sandbox", "work", "documents", "downloads", "downloads/torrents"],
+  rmrf: ["Desktop", "Documents", "Downloads", "Music", "Pictures", "Templates", "Videos"],
+  cmd: { exec: () => $`xdg-user-dirs-update`, name: "xdg dirs update", quiet: true },
+} satisfies XdgArgs;
+
+export const repoArgs = [
+  {
+    exec: () =>
+      $`git clone https://github.com/mkvlrn/arch-setup ${path.join(os.homedir(), "repos", "arch-setup")}`,
+    name: "clone repo",
+    quiet: true,
+  },
+] satisfies Command[];
--- main.ts
+++ main.ts
@@ -1,35 +1,42 @@
 import process from "node:process";
 import { $ } from "bun";
-import { updateConfsArgs, xdgArgs, yayArgs } from "./main-data";
+import { baseArgs, pkgArgs, repoArgs, updateConfsArgs, xdgArgs, yayArgs } from "./main-data";
 import { updateConfs } from "./scripts/update-confs";
 import { xdg } from "./scripts/xdg";
-import { yay } from "./scripts/yay";
 import { formatError } from "./utils/format-error";
+import { runShellCommands } from "./utils/run-shell-commands";
 
-await $`sudo -v`;
+async function main() {
+  await $`sudo -v`;
 
-const keepalive = Bun.spawn(["sh", "-c", "while sleep 30; do sudo -n true || exit; done"], {
-  stdout: "ignore",
-  stderr: "ignore",
-});
+  const keepalive = Bun.spawn(["sh", "-c", "while sleep 30; do sudo -n true || exit; done"], {
+    stdout: "ignore",
+    stderr: "ignore",
+  });
 
-const scripts = [
-  [() => xdg(xdgArgs), "Configure XDG user directories"],
-  [() => updateConfs(updateConfsArgs), "Configure pacman"],
-  [() => yay(yayArgs), "Install yay and update mirrors"],
-] as const;
+  const scripts = [
+    [() => updateConfs(updateConfsArgs), "Configure pacman"],
+    [() => runShellCommands(baseArgs), "Install minimal packages to build yay"],
+    [() => runShellCommands(yayArgs), "Install yay and update mirrors"],
+    [() => runShellCommands(pkgArgs), "Install packages with yay and do cleanup"],
+    [() => runShellCommands(repoArgs), "Clone arch-setup repo to ~/repos/arch-setup"],
+    [() => xdg(xdgArgs), "Configure XDG user directories"],
+  ] as const;
 
-try {
-  for await (const [index, script] of scripts.entries()) {
-    const [cmd, label] = script;
-    console.log(`[${index + 1}/${scripts.length}] ${label}`);
-    const result = await cmd();
-    if (result.isError) {
-      console.error(formatError(result.error));
-      process.exit(1);
+  try {
+    for await (const [index, script] of scripts.entries()) {
+      const [cmd, label] = script;
+      console.log(`[${index + 1}/${scripts.length}] ${label}`);
+      const result = await cmd();
+      if (result.isError) {
+        console.error(formatError(result.error));
+        process.exit(1);
+      }
     }
+  } finally {
+    console.info("Done.");
+    keepalive.kill();
   }
-} finally {
-  console.info("Done.");
-  keepalive.kill();
 }
+
+main();

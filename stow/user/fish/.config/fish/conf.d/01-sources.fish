60587fa9b58795fe6b3a9b917284fc5f7239e1bc
mkvlrn <dev@mkvlrn.cc>
2026-08-23 14:30:14 -0300

    fix: omp theme fallback

--- stow/user/fish/.config/fish/conf.d/01-sources.fish
+++ stow/user/fish/.config/fish/conf.d/01-sources.fish
@@ -2,7 +2,8 @@
 mise activate fish | source
 
 # oh-my-posh
-oh-my-posh init fish --config ~/repos/ts-tools/packages/config/src/mkvlrn.omp.jsonc | source
+set omp_config ~/repos/ts-tools/packages/config/src/mkvlrn.omp.jsonc; test -f $omp_config; or set omp_config https://raw.githubusercontent.com/mkvlrn/ts-tools/main/packages/config/src/mkvlrn.omp.jsonc
+oh-my-posh init fish --config $omp_config | source
 
 # keychain, .ssh
 if status is-interactive

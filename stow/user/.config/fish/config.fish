44c517a42f95b2d7752e2793af2e3ae505b87f73
mkvlrn <dev@mkvlrn.cc>
2026-09-12 08:05:32 -0300

    chore: bump stow

--- stow/user/.config/fish/config.fish
+++ stow/user/.config/fish/config.fish
@@ -11,10 +11,6 @@ if status is-interactive
     test -f $omp_config; or set omp_config https://raw.githubusercontent.com/mkvlrn/ts-tools/main/packages/config/src/mkvlrn.omp.jsonc
     oh-my-posh init fish --config $omp_config | source
 
-    # keychain, .ssh
-    keychain --eval --quiet | source
-    ssh-add -q ~/.ssh/dev </dev/null
-
     # aliases
     # zed
     alias zed zeditor
--- stow/user/.config/fish/functions/upgrayedd.fish
+++ stow/user/.config/fish/functions/upgrayedd.fish
@@ -1,9 +1,16 @@
 function upgrayedd --description "Update system, mise, tools, and completions"
+    set -l old_pwd $PWD
+    cd ~
+    or return
+
     yay
-    and mise self-update -y; or mise self-update -y # fail tolerance-ish
+    and mise self-update -y; or mise self-update -y
+    and mise prune -y
     and mise cache clear
     and mise upgrade -b
     and mise completion fish >~/.config/fish/completions/mise.fish
     and gh completion -s fish >~/.config/fish/completions/gh.fish
     and glab completion -s fish >~/.config/fish/completions/glab.fish
+
+    cd $old_pwd
 end

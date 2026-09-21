f511bdb425c36e2200d21307e6395d2261501038
mkvlrn <dev@mkvlrn.cc>
2026-09-21 17:29:53 -0300

    fix: Check Proton remote type from full rclone config

--- internal/checks/rclone.go
+++ internal/checks/rclone.go
@@ -18,8 +18,14 @@ func RcloneProton(cfg *config.Config) setup.Check {
 		Run: func(ctx context.Context) error {
 			_, err := shell.Run(ctx, []shell.Command{{
 				Name: "get rclone Proton Drive type",
-				Path: misePath,
-				Args: []string{"exec", "--", "rclone", "config", "get", "proton", "type"},
+				Path: "sh",
+				Args: []string{
+					"-c",
+					`set -eu
+"$1" exec -- rclone config show proton | grep -q '^type[[:space:]]*=[[:space:]]*protondrive$'`,
+					"check-rclone-proton",
+					misePath,
+				},
 			}})
 
 			return err
